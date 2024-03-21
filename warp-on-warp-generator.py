0ca08397c46f0c9da90f52dc5f992f142e8de0fa
DrunkLeen <reza.df.x@gmail.com>
2024-03-21 08:54:38 +0100

    CLI comments added

--- warp-on-warp-generator.py
+++ warp-on-warp-generator.py
@@ -1,12 +1,18 @@
-import os
-import requests
 import json
+import os
 import random
 import string
 
+import requests
+
+GREEN = "\033[92m"
+RED = "\033[91m"
+YELLOW = "\033[93m"
+RESET = "\033[0m"
+
 
 def generate_random_string() -> str:
-    return ''.join(random.choices(string.ascii_letters + string.digits, k=8))
+    return "".join(random.choices(string.ascii_letters + string.digits, k=8))
 
 
 def load_json_template() -> json:
@@ -31,33 +37,54 @@ def register_warp_account() -> json:
         print("Error registering")
 
 
-if __name__ == '__main__':
-    json_file_to_save = load_json_template()
-    first_account = register_warp_account()
-    second_account = register_warp_account()
+if __name__ == "__main__":
+    try:
+        json_file_to_save = load_json_template()
+        print(f"[+] {GREEN}Config Template loaded.{RESET}")
+        first_account = register_warp_account()
+        print(f"[+] {GREEN}First account registered.{RESET}")
+        second_account = register_warp_account()
+        print(f"[+] {GREEN}Second account registered.{RESET}")
 
-    json_file_to_save["outbounds"][2]["local_address"][1] = first_account['local_address'][1]
-    json_file_to_save["outbounds"][2]["private_key"] = first_account['private_key']
-    json_file_to_save["outbounds"][2]["reserved"] = first_account['reserved']
+        json_file_to_save["outbounds"][2]["local_address"][1] = first_account[
+            "local_address"
+        ][1]
+        json_file_to_save["outbounds"][2]["private_key"] = first_account["private_key"]
+        json_file_to_save["outbounds"][2]["reserved"] = first_account["reserved"]
 
-    json_file_to_save["outbounds"][3]["local_address"][1] = second_account['local_address'][1]
-    json_file_to_save["outbounds"][3]["private_key"] = second_account['private_key']
-    json_file_to_save["outbounds"][3]["reserved"] = second_account['reserved']
+        json_file_to_save["outbounds"][3]["local_address"][1] = second_account[
+            "local_address"
+        ][1]
+        json_file_to_save["outbounds"][3]["private_key"] = second_account["private_key"]
+        json_file_to_save["outbounds"][3]["reserved"] = second_account["reserved"]
 
-    if input("Do you want to use custom clean IP? (y/N): ").lower() == "y":
-        ip = input("Enter clean IP address: ").strip()
-        port = int(input("Enter clean the port of the clean IP: ").strip())
-        
-        json_file_to_save["outbounds"][3]["server"] = ip
-        json_file_to_save["outbounds"][3]["server_port"] = port
-        json_file_to_save["outbounds"][3]["server"] = ip
-        json_file_to_save["outbounds"][3]["server_port"] = port
-    else:
-        json_file_to_save["outbounds"][2]["server"] = "162.159.193.10"
-        json_file_to_save["outbounds"][2]["server_port"] = 2408
-        json_file_to_save["outbounds"][3]["server"] = "162.159.193.10"
-        json_file_to_save["outbounds"][3]["server_port"] = 2408
+        if (
+            input(
+                f"{YELLOW}[+] Do you want to use custom clean IP? (y/N): {RESET}"
+            ).lower()
+            == "y"
+        ):
+            ip = input("Enter clean IP address: ").strip()
+            port = int(input("Enter clean the port of the clean IP: ").strip())
 
-    check_output_dir()
-    with open(f"output/{generate_random_string()}.json", "w") as f:
-        f.write(json.dumps(json_file_to_save))
+            json_file_to_save["outbounds"][3]["server"] = ip
+            json_file_to_save["outbounds"][3]["server_port"] = port
+            json_file_to_save["outbounds"][3]["server"] = ip
+            json_file_to_save["outbounds"][3]["server_port"] = port
+        else:
+            json_file_to_save["outbounds"][2]["server"] = "162.159.193.10"
+            json_file_to_save["outbounds"][2]["server_port"] = 2408
+            json_file_to_save["outbounds"][3]["server"] = "162.159.193.10"
+            json_file_to_save["outbounds"][3]["server_port"] = 2408
+
+        check_output_dir()
+        config_name = str(generate_random_string()) + ".json"
+        with open(f"output/{config_name}", "w") as f:
+            f.write(json.dumps(json_file_to_save))
+
+        print(
+            f"\n[+] {GREEN}Config was successfully created!{RESET}\n[+] {GREEN}Generated config saved:\n      {RESET}{os.getcwd()}/output/{config_name}\n"
+        )
+
+    except Exception as e:
+        print(f"{RED}[+] Couldn't make the config.\nThere was an error:{RESET}\n{e}")
